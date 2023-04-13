e17cb0a077ee128fd58e5add842f0caeff13a3c7
smoncada320 <112918343+smoncada320@users.noreply.github.com>
2023-04-12 21:52:12 -0500

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,14 @@
+#To define a function that take two integers and return the sum of those two numbers
+def solution(param1, param2):
+    return param1 + param2
+    
+#initializing the variables    
+num1= 1
+num2= 2
+
+#function calling and store the result into sum_of_twonumbers
+sum_of_twonumbers = solution(num1,num2)
+
+#To print the result
+print("Sum of {0} and {1} is {2};" .format(num1, num2, sum_of_twonumbers))
+
